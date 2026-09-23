2814f160763840c8bdbdb2d97946bded6eca1106
sisafa-collab <hnbra.execucaofinanceira@gmail.com>
2026-09-23 14:06:55 -0300

    Update file names for CISSFA to 2026 version

--- app.py
+++ app.py
@@ -51,8 +51,8 @@ st.markdown("""
 @st.cache_data
 def carregar_tabela_referencia():
     # Tenta carregar XLSX ou CSV de forma inteligente direto do GitHub
-    arquivo_xlsx = "CISSFA.xlsx"
-    arquivo_csv = "CISSFA.csv"
+    arquivo_xlsx = "CISSFA-2026.xlsx"
+    arquivo_csv = "CISSFA-2026.csv"
     
     df_ref = None
     
